a2e35cf448aad15ef950649a4efaffd2b4ead63a
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2025-08-06 12:58:58 -0300

    Practica de String en Typescript

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+// Practica de String / cadena de caracteres
+// let batman:string = "Batman";
+// let linternaVerde:string = 'Linterna Verde';
+// let volcanNegro:string = `Volcan Negro`;
+Object.defineProperty(exports, "__esModule", { value: true });
+// console.log(batman);
+// console.log(linternaVerde);
+// console.log(volcanNegro);
+// let concatenar:string = `Los heroes son: ${ batman}, ${linternaVerde}, ${volcanNegro}`; 
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,10 @@
+// Practica de String / cadena de caracteres
+// let batman:string = "Batman";
+// let linternaVerde:string = 'Linterna Verde';
+// let volcanNegro:string = `Volcan Negro`;
+
+// console.log(batman);
+// console.log(linternaVerde);
+// console.log(volcanNegro);
+
+// let concatenar:string = `Los heroes son: ${ batman}, ${linternaVerde}, ${volcanNegro}`; 
